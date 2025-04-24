5229b0c7c5e65256611497f50b063b451b55202c
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-24 14:03:16 +0200

    feat: es 2 completed

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,29 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array
+ contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
+
 // Dichiara la funzione qui.
-
-
+function nameWithLetter(names) {
+    let newNames = [];
+    console.log(newNames);
+    for(let i = 0; i < names.length; i++ ){
+        let curName = names[i];
+        console.log(curName);
+        for ( let i = 0; i < curName.length; i++)
+           curLetter = curName[i - (curName.length - 1)];
+        if(curLetter === "A"){
+            newNames.push(curName);
+        }
+        }
+     
+}
+    
+    
 // Invoca la funzione qui e stampa il risultato in console
 
-
+nameWithLetter(names);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
