85c3c2371fd74b4a90b196f821865d7d2af61c6a
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-24 14:15:23 +0200

    feat:es 3 completed

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,21 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function vowes(word){
+    let wordVowes = [];
+    console.log(wordVowes);
+    for( let i = 0; i < word.length; i++){
+        let curLetter = word[i];
+        console.log(curLetter);
+        if (curLetter === "a" || curLetter === "e" || curLetter === "i" || curLetter === "o" || curLetter === "u"){
+            wordVowes.push(curLetter);
+        }
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+vowes(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
