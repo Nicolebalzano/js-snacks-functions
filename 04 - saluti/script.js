ed5e97d493b77d9f5c480399fd8192902e9a015a
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-24 14:27:55 +0200

    feat:es 4 completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = prompt("come ti chiami?");
 
 
 // Dichiara la funzione qui.
-
+function helloName (name) {
+    let message = "Ciao" + " " + name + "!"
+        console.log(message);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+helloName(name);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
