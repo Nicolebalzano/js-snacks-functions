17bdad12b6892dfc9f87fea1f9ce6a25a9b39c42
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-26 22:02:37 +0200

    feat:esercizio 5 bonus completed

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,27 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const name = 'Nicole';
 
 
 // Dichiara la funzione qui.
-
+function greetings(name) { 
+    const date = new Date ();
+    date.getHours();
+    if(date <= 13) {
+        let message = "Buongiorno" + " " + name;
+        console.log(message);
+    } else if (date > 13 && date < 17) {
+        let message = "Buon pomeriggio" + " " + name;
+        console.log(message);
+    } else if (date > 17) {
+        let message = "Buona sera" + " " + name;
+        console.log(message);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greetings(name);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
